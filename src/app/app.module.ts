98022d09bdd980e7ec7ccb2195f3261eaae3c2e8
Mario Rosa <mario309060@gmail.com>
2022-01-18 17:09:35 -0300

    Module registered in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeanDeviceModule } from './lean-device/lean-device.module';
 import { CategoryModule } from './category/category.module';
+import { DeviceModule } from './device/device.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { CategoryModule } from './category/category.module';
     BrowserModule,
     LeanDeviceModule,
     CategoryModule,
+    DeviceModule,
     AppRoutingModule // sempre manter por último.
   ],
   providers: [],
