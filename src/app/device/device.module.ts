4472ea647b76c3ad3ff2d7b89b049ad50cd29b8f
Mario Rosa <mario309060@gmail.com>
2022-01-18 17:23:06 -0300

    Criado o DeviceService e importado no providers[] no Device module

--- src/app/device/device.module.ts
+++ src/app/device/device.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { DeviceService } from './services/device.service';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    DeviceService
   ]
 })
 export class DeviceModule { }
